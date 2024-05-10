2b3a759e0bf798caef1abd1df1d3cc6ea261894c
Muhammad Huzaifa Rauf <mhuzaifarauf13@gmail.com>
2024-05-10 12:23:42 +0500

    feat:fetched data from database and dispayed on the home page

--- components/TopicsList.tsx
+++ components/TopicsList.tsx
@@ -3,22 +3,43 @@ import RemoveBtn from "./RemoveBtn";
 import Link from "next/link";
 import { HiPencilAlt } from "react-icons/hi";
 
-export default function TopicsList() {
+const getBlogs = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/blogs", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Something went wrong");
+    }
+    return res.json();
+  } catch (error) {
+    console.log("Error fetching blog:", error);
+  }
+};
+export default async function TopicsList() {
+  const blogs = await getBlogs();
+
   return (
     <>
-      <div className="p-4 border border-slate-300 my-3 flex justify-between rounded-md gap-5 items-start">
-        <div className="">
-          <h2 className="font-semibold text-2xl">Blog Title</h2>
-          <div>Blog Description</div>
-        </div>
+      {blogs?.map((item: any) => (
+        <div
+          key={item._id}
+          className="p-4 border border-slate-300 my-3 flex justify-between rounded-md gap-5 items-start"
+        >
+          <div className="">
+            <h2 className="font-semibold text-2xl">{item.title}</h2>
+            <div>{item.description}</div>
+          </div>
 
-        <div className="flex gap-2">
-          <RemoveBtn />
-          <Link href="/editTopic/123">
-            <HiPencilAlt size={24} />
-          </Link>
+          <div className="flex gap-2">
+            <RemoveBtn />
+            <Link href={`/editTopic/${item._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
         </div>
-      </div>
+      ))}
     </>
   );
 }
