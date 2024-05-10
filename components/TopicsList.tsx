fca501887ed8153ede9845df426e2e8406d55335
Muhammad Huzaifa Rauf <mhuzaifarauf13@gmail.com>
2024-05-10 18:11:28 +0500

    feat:CRUD completed

--- components/TopicsList.tsx
+++ components/TopicsList.tsx
@@ -1,49 +1,3 @@
-// import React from "react";
-// import RemoveBtn from "./RemoveBtn";
-// import Link from "next/link";
-// import { HiPencilAlt } from "react-icons/hi";
-
-// const getBlogs = async () => {
-//   try {
-//     const res = await fetch("http://localhost:3000/api/blogs", {
-//       cache: "no-store",
-//     });
-
-//     if (!res.ok) {
-//       throw new Error("Something went wrong");
-//     }
-//     return res.json();
-//   } catch (error) {
-//     console.log("Error fetching blog:", error);
-//   }
-// };
-// export default async function TopicsList() {
-//   const blogs = await getBlogs();
-
-//   return (
-//     <>
-//       {blogs?.map((item: any) => (
-//         <div
-//           key={item._id}
-//           className="p-4 border border-slate-300 my-3 flex justify-between rounded-md gap-5 items-start"
-//         >
-//           <div className="">
-//             <h2 className="font-semibold text-2xl">{item.title}</h2>
-//             <div>{item.description}</div>
-//           </div>
-
-//           <div className="flex gap-2">
-//             <RemoveBtn id={item._id} />
-//             <Link href={`/editTopic/${item._id}`}>
-//               <HiPencilAlt size={24} />
-//             </Link>
-//           </div>
-//         </div>
-//       ))}
-//     </>
-//   );
-// }
-
 "use client";
 import React, { useState, useEffect } from "react";
 import RemoveBtn from "./RemoveBtn";
@@ -59,7 +13,7 @@ export default function TopicsList() {
     <>
       {loading ? (
         <Loader />
-      ) : (
+      ) : blogs && blogs.length > 0 ? (
         blogs?.map((item: any) => (
           <div
             key={item._id}
@@ -78,6 +32,10 @@ export default function TopicsList() {
             </div>
           </div>
         ))
+      ) : (
+        <h3 className="text-center">
+          No blogs have been added by the user yet
+        </h3>
       )}
     </>
   );
